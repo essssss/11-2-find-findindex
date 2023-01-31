bbae3852da108c31841552594198b61d3cfa00a7
essssss <srmcnamara7@gmail.com>
2023-01-31 15:48:59 -0500

    exercieses all done

--- exercises/find-findindex.js
+++ exercises/find-findindex.js
@@ -1,18 +1,21 @@
 /* 
 Write a function called `findUserByUsername` which accepts an array of objects, each with a key of username, and a string. The function should return the first object with the key of username that matches the string passed to the function. If the object is not found, return undefined. 
-
+*/
 const users = [
-  {username: 'mlewis'},
-  {username: 'akagen'},
-  {username: 'msmith'}
+   { username: 'mlewis' },
+   { username: 'akagen' },
+   { username: 'msmith' },
 ];
-
+/*
 findUserByUsername(users, 'mlewis') // {username: 'mlewis'}
 findUserByUsername(users, 'taco') // undefined
 */
 
-
-function findUserByUsername(usersArray, username) {}
+function findUserByUsername(usersArray, name) {
+   return usersArray.find(function (user) {
+      return user['username'] === name;
+   });
+}
 
 /*
 Write a function called `removeUser` which accepts an array of objects, each with a key of username, and a string. The function should remove the object from the array. If the object is not found, return undefined. 
@@ -27,4 +30,12 @@ removeUser(users, 'akagen') // {username: 'akagen'}
 removeUser(users, 'akagen') // undefined
 */
 
-function removeUser(usersArray, username) {}
+function removeUser(usersArray, username) {
+   const removeUserIdx = usersArray.findIndex(function (user) {
+      return user['username'] === username;
+   });
+   if (removeUserIdx === -1) return undefined;
+   const removedUser = usersArray[removeUserIdx];
+   usersArray.splice(removeUserIdx, 1);
+   return removedUser;
+}
